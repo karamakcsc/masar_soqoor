649e37a8d8ea3504759fb43d3a0001a1287d55ab
karamakcsc <karamakcsc@hotmail.com>
2023-01-08 14:23:07 +0000

    update permission

--- masar_soqoor/custom/payment_enrty/payment_entry.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on("Payment Entry", "onload", function(frm) {
-if(user=="w.hussain@sattcsa.com"){
-   {
-           frappe.msgprint("You cannot makes changes to this item.");
-   }
- }
-});
--- masar_soqoor/custom/payment_entry/payment_entry.js
+++ masar_soqoor/custom/payment_entry/payment_entry.js
@@ -9,8 +9,11 @@ if(user=="w.hussain@sattcsa.com"){
        df.read_only=1;
        var df=frappe.meta.get_docfield("Payment Entry", "party_type",frm.doc.name);
        df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "mode_of_payment",frm.doc.name);
+       df.read_only=1;
        frm.set_value('naming_series', 'ACC-PAY-.YYYY.-')
        frm.set_value('payment_type', 'Receive')
+       frm.set_value('mode_of_payment', 'Cash')
        frm.set_value('paid_to', '111002 - صندوق المعرض - SATC')
        frm.set_value('party_type', 'Customer')
        frm.refresh_fields();
