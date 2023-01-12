a02dbb19d9bdf1fe8fe26bddaa1e04fd45686240
karamakcsc <karamakcsc@hotmail.com>
2023-01-12 09:06:22 +0000

    field INV

--- masar_soqoor/custom/sales_invoice/sales_invoice.js
+++ masar_soqoor/custom/sales_invoice/sales_invoice.js
@@ -1,31 +1,36 @@
-////// Fetching Employee with Filter ///// Start ///Siam
-// frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
-//     if (frm.doc.payment_type =="Cash") {
-//       frm.doc.refresh_fields();
-//       cur_frm.fields_dict['customer'].get_query = function(doc) {
-//       	return {
-//       		filters: {
-//       			"customer_group": "Cash Customer"
-//       		}
-//       	}
-//       }
-//
-//   }
-// });
-////// Fetching Employee with Filter ///// END ///Siam
+frappe.ui.form.on("Sales Invoice","refresh", function(frm) {
+     frm.toggle_display("naming_series", false);
+     frm.toggle_display("is_debit_note", false);
+     frm.toggle_display("time_sheet_list", false);
+     frm.toggle_display("edit_printing_settings", false);
+     frm.toggle_display("shipping_rule", false);
+     frm.toggle_display("loyalty_points_redemption", false);
+     frm.toggle_display("printing_details", false);
+     frm.toggle_display("sales_team_section_break", false);
+     frm.toggle_display("subscription_section", false);
+     frm.toggle_display("update_billed_amount_in_sales_order", false);
+     frm.toggle_display("more_information", false);
+});
 
+frappe.ui.form.on("Sales Invoice", {
+  is_return: function (frm) {
+    if (frm.doc.is_return == 1) {
+    frm.set_value('naming_series', 'SINV-RET-.YYYY.-')
+    refresh_field("naming_series");
+  }
+  else {
+  frm.set_value('naming_series', 'SINV-.YYYY.-')
+    refresh_field("naming_series");
+  }
 
-// frappe.ui.form.on("Sales Invoice", {
-// 	refresh: function(frm) {
-//     if (frm.doc.payment_type =="Cash") {
-//       frm.doc.refresh_fields();
-// 		frm.set_query("customer", function() {
-// 			return {
-// 				filters: [
-// 					["Customer","customer_group", "=", "Cash Customer"]
-// 				]
-// 			}
-// 		});
-//   }
-// 	}
-// });
+  }
+});
+
+frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
+       var df=frappe.meta.get_docfield("Sales Invoice", "naming_series",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Sales Invoice", "pos_profile",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Sales Invoice", "debit_to",frm.doc.name);
+       df.read_only=1;
+});
