855ebdef8a998272de14de88118ae763aece43d1
karamakcsc <karamakcsc@hotmail.com>
2023-03-01 09:55:29 +0000

    payment entry

--- masar_soqoor/custom/payment_entry/payment_entry.js
+++ masar_soqoor/custom/payment_entry/payment_entry.js
@@ -47,3 +47,31 @@ if(user=="a.rafique@sattcsa.com"){
    }
  }
 });
+
+frappe.ui.form.on("Payment Entry", "onload", function(frm) {
+if(user=="s.halawi@sattcsa.com"){
+   {
+       var df=frappe.meta.get_docfield("Payment Entry", "naming_series",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "payment_type",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "paid_to",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "party_type",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "mode_of_payment",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "posting_date",frm.doc.name);
+       df.read_only=1;
+       var df=frappe.meta.get_docfield("Payment Entry", "paid_amount",frm.doc.name);
+       df.read_only=1;
+
+       frm.set_value('naming_series', 'ACC-PAY-.YYYY.-')
+       frm.set_value('payment_type', 'Receive')
+       frm.set_value('mode_of_payment', 'Cash-المعرض')
+       frm.set_value('paid_to', '111002 - صندوق المعرض - SATC')
+       frm.set_value('party_type', 'Customer')
+       frm.refresh_fields();
+   }
+ }
+});
