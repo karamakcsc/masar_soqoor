7489b6a92173f1d4f9b6e8b742a55c72862c462d
karamakcsc <karamakcsc@hotmail.com>
2023-12-17 21:15:20 +0300

    permission update_2

--- masar_soqoor/custom/sales_order/sales_order.js
+++ masar_soqoor/custom/sales_order/sales_order.js
@@ -257,7 +257,7 @@ frappe.ui.form.on('Sales Order', {
 
 frappe.ui.form.on('Sales Order', {
     refresh(frm) {
-        if ( !frappe.user.has_role('System Manager')) {
+        if ( !frappe.user.has_role('System Manager') && !frappe.user.has_role('Showrrom User')) {
         setTimeout(() => {
             // frm.remove_custom_button('Update Items');
             frm.remove_custom_button('Purchase Order', 'Create');
@@ -277,3 +277,23 @@ frappe.ui.form.on('Sales Order', {
 });
 
 
+frappe.ui.form.on('Sales Order', {
+    refresh(frm) {
+        if ( !frappe.user.has_role('System Manager') && frappe.user.has_role('Showrrom User')) {
+        setTimeout(() => {
+            // frm.remove_custom_button('Update Items');
+            frm.remove_custom_button('Purchase Order', 'Create');
+            frm.remove_custom_button('Subscription', 'Create');
+            // frm.remove_custom_button('Sales Invoice', 'Create');
+            frm.remove_custom_button('Delivery Note', 'Create');
+            frm.remove_custom_button('Sales Invoice', 'Create');
+            frm.remove_custom_button('Material Request', 'Create');
+            frm.remove_custom_button('Request for Raw Materials', 'Create');
+            frm.remove_custom_button('Project', 'Create');
+            frm.remove_custom_button('Pick List', 'Create');
+            frm.remove_custom_button('Work Order', 'Create');
+            frm.remove_custom_button('Quotation','Get Items From');
+        }, 10);
+    }
+}
+});
