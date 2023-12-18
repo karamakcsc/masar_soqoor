f62f8787ed931f64d6501ba1d3ea403b3c82436e
karamakcsc <karamakcsc@hotmail.com>
2023-12-18 04:05:34 +0300

    permission update

--- masar_soqoor/custom/sales_invoice/sales_invoice.js
+++ masar_soqoor/custom/sales_invoice/sales_invoice.js
@@ -171,58 +171,7 @@ frappe.ui.form.on("Sales Invoice","onload", function(frm) {
 
   if (frappe.user.has_role('Showroom User') && frappe.user.has_role('Stock User')) {
 
-    // frm.toggle_display("naming_series", false);
-    // frm.toggle_display("set_posting_time", false);
-    // frm.toggle_display("permit_no", false);
-    // frm.toggle_display("vat_emirate", false);
-    // frm.toggle_display("more_info", false);
-    // frm.toggle_display("incoterm", false);
-    // frm.toggle_display("section_break_88", false);
-    // frm.toggle_display("advances_section", false);
-    // frm.toggle_display("write_off_section", false);
-    // frm.toggle_display("edit_printing_settings", false);
-    // frm.toggle_display("vat_section", false);
-    // frm.toggle_display("accounting_dimensions_section", false);
-    // frm.toggle_display("currency_and_price_list", false);
-    // frm.toggle_display("more_info", false);
-    // frm.toggle_display("sales_team_section_break", false);
-    // frm.toggle_display("more_information", false);
-    // frm.toggle_display("taxes", false);
-    // frm.toggle_display("disable_rounded_total", false);
-    // frm.toggle_display("sec_tax_breakup", false);
-    // frm.toggle_display("packing_list", false);
-    // frm.toggle_display("use_company_roundoff_cost_center", false);
-    // frm.toggle_display("redeem_loyalty_points", false);
-    // frm.toggle_display("shipping_rule", false);
-    // frm.toggle_display("payment_schedule_section", false);
-    
-    
-    // var df=frappe.meta.get_docfield("Sales Invoice", "update_stock",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "is_pos",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "tax_category",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "taxes_and_charges",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "taxes",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "set_warehouse",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "is_debit_note",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "update_billed_amount_in_delivery_note",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "update_billed_amount_in_sales_order",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "return_against",frm.doc.name);
-    // df.read_only=1;
-    // var df=frappe.meta.get_docfield("Sales Invoice", "is_return",frm.doc.name);
-    // df.read_only=1;
-
     frm.set_value('is_pos', 1);
-    // frm.set_value('tax_category', 'VAT_15');
-    // frm.set_value('taxes_and_charges', 'KSA VAT 15% - SATC');
     frm.set_value('update_stock', 1);
     frm.set_value('pos_profile','Standard');
     
@@ -278,63 +227,10 @@ frappe.ui.form.on("Sales Invoice", {
 frappe.ui.form.on("Sales Invoice","onload", function(frm) {
 
     if (frappe.user.has_role('Sales User')) {
-  
-      // frm.toggle_display("naming_series", false);
-      // frm.toggle_display("set_posting_time", false);
-      // frm.toggle_display("permit_no", false);
-      // frm.toggle_display("vat_emirate", false);
-      // frm.toggle_display("more_info", false);
-      // frm.toggle_display("incoterm", false);
-      // frm.toggle_display("section_break_88", false);
-      // frm.toggle_display("advances_section", false);
-      // frm.toggle_display("write_off_section", false);
-      // frm.toggle_display("edit_printing_settings", false);
-      // frm.toggle_display("vat_section", false);
-      // frm.toggle_display("accounting_dimensions_section", false);
-      // frm.toggle_display("currency_and_price_list", false);
-      // frm.toggle_display("more_info", false);
-      // frm.toggle_display("sales_team_section_break", false);
-      // frm.toggle_display("more_information", false);
-      // frm.toggle_display("taxes", false);
-      // frm.toggle_display("disable_rounded_total", false);
-      // frm.toggle_display("sec_tax_breakup", false);
-      // frm.toggle_display("packing_list", false);
-      // frm.toggle_display("use_company_roundoff_cost_center", false);
-      // frm.toggle_display("redeem_loyalty_points", false);
-      // frm.toggle_display("shipping_rule", false);
-      // frm.toggle_display("payment_schedule_section", false);
-      
-     
-      
-      // var df=frappe.meta.get_docfield("Sales Invoice", "update_stock",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "is_pos",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "tax_category",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "taxes_and_charges",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "taxes",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "is_debit_note",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "update_billed_amount_in_delivery_note",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "update_billed_amount_in_sales_order",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "return_against",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "is_return",frm.doc.name);
-      // df.read_only=1;
+
       var df=frappe.meta.get_docfield("Sales Invoice", "payment_type",frm.doc.name);
       df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "naming_series",frm.doc.name);
-      // df.read_only=1;
-      // var df=frappe.meta.get_docfield("Sales Invoice", "territory",frm.doc.name);
-      // df.read_only=1;
-      
-      // frm.set_value('tax_category', 'VAT_15');
-      // frm.set_value('taxes_and_charges', 'KSA VAT 15% - SATC');
+
       frm.set_value('update_stock', 0);
       frm.set_value('payment_type',"On Account");
       frm.set_value('is_pos', 0);
