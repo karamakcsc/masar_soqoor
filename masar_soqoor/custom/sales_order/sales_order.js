a9cee179a06216b8d8ee051aa5a33016959a9eb6
karamakcsc <karamakcsc@hotmail.com>
2023-12-18 03:16:35 +0300

    permission update

--- masar_soqoor/custom/sales_order/sales_order.js
+++ masar_soqoor/custom/sales_order/sales_order.js
@@ -102,7 +102,7 @@ frappe.ui.form.on('Sales Order', {
 
 frappe.ui.form.on('Sales Order', {
     refresh(frm) {
-        if ( !frappe.user.has_role('System Manager') && !frappe.user.has_role('Showroom User') && frappe.user.has_role('Sales User') && frappe.user.has_role('Sales Manager')) {
+        if ( !frappe.user.has_role('System Manager') && frappe.user.has_role('Sales User') && frappe.user.has_role('Sales Manager')) {
         setTimeout(() => {
             // frm.remove_custom_button('Update Items');
             frm.remove_custom_button('Purchase Order', 'Create');
