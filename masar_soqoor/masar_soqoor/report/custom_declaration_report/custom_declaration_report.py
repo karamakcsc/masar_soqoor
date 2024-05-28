c6ddab1f65da9a39946294ed6127a2c27ec1d8ba
karamakcsc <karamakcsc@hotmail.com>
2024-05-28 12:37:55 +0300

    report

--- masar_soqoor/masar_soqoor/report/custom_declaration_report/custom_declaration_report.py
+++ masar_soqoor/masar_soqoor/report/custom_declaration_report/custom_declaration_report.py
@@ -18,25 +18,28 @@ def get_data(filters):
 
     data = frappe.db.sql(f"""
         SELECT
-            tpi.supplier,
-            tpi.posting_date,
-            tpi.reference_no,
-            tpi.supplier_name,
-            tpi.name,
-            tpi.custom_cus_dec_date,
-            tpi.custom_cus_dec_no,
-            tpi.custom_port_name,
-            tpi.custom_cus_dec_amount,
-            tpi.custom_cus_dec_fee,
-            tpi.custom_cus_dec_tax,
-            tpi.base_total
-        FROM `tabPurchase Invoice` tpi 
-        WHERE (tpi.posting_date BETWEEN %s AND %s)
-            tpi.tax_category in ('VAT_PI' , 'VAT_I') 
-            AND tpi.taxes_and_charges in ('VAT_PI' , 'VAT_I')
+    tpi.supplier,
+    tpi.posting_date,
+    tpi.reference_no,
+    tpi.supplier_name,
+    tpi.name,
+    tpi.custom_cus_dec_date,
+    tpi.custom_cus_dec_no,
+    tpi.custom_port_name,
+    tpi.custom_cus_dec_amount,
+    tpi.custom_cus_dec_fee,
+    tpi.custom_cus_dec_tax,
+    tpi.base_total
+FROM 
+    `tabPurchase Invoice` tpi 
+WHERE 
+    (tpi.posting_date BETWEEN '{_from}' AND '{to}')
+    AND tpi.tax_category IN ('VAT_PI', 'VAT_I') 
+    AND tpi.taxes_and_charges IN ('VAT_PI', 'VAT_I')
+
             AND tpi.docstatus = 1  {conditions}
         ORDER BY tpi.posting_date DESC;
-        """, (_from, to))
+        """)
 
     return data
 
